b1c3d09d22bd69e479c6f907703a605f6f195270
d-coder <82116626+d-realcoder@users.noreply.github.com>
2021-11-14 00:22:41 +0530

    Modified controllers

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -5,24 +5,24 @@ import mongoose from 'mongoose';
 import User from "../models/user.js"
 
 export const signup = async (req, res) => {
-    const { email, password, confirmPassword, firstName, lastName } = req.body;
+    const { email, password, confirmPassword, firstName, lastName, phone } = req.body;
 
     try {
         const existingUser = await User.findOne({ email });
 
         if (existingUser) {
-            res.status(400).json({ message: "User already exist!" });
+            return res.status(400).json({ message: "User already exist!" });
         }
 
         if (password !== confirmPassword) return res.status(400).json({ message: "Passwords don't match! " });
         const hashedPassword = await bcrypt.hash(password, 12);
-        const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
+        const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}`, phone });
         const token = jwt.sign({ email: result.email, id: result._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
         mongoose.connection.close()
-        res.status(200).json({ result, token });
+        return res.status(200).json({ result, token });
     } catch (error) {
         console.log(error);
-        res.status(500).json({ message: "Something went wrong!" })
+        return res.status(500).json({ message: "Something went wrong!" })
     }
 
 }
@@ -34,7 +34,7 @@ export const signin = async (req, res) => {
         const existingUser = await User.findOne({ email });
 
         if (!existingUser) {
-            res.status(404).json({ message: "User doesn't exist!" });
+            return res.status(404).json({ message: "User doesn't exist!" });
         }
 
         const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
@@ -42,6 +42,6 @@ export const signin = async (req, res) => {
         const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
         res.status(200).json({ result: existingUser, token });
     } catch (error) {
-        res.status(500).json({ message: "Something went wrong!" })
+        return res.status(500).json({ message: "Something went wrong!" })
     }
 }
